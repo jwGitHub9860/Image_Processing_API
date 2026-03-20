8f082c25eb8e117014479fb861a958fe6c6595f9
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2026-03-20 16:56:38 -0400

    feat: Add tests for API endpoints using Jasmine and SuperTest
    Replace temporary code with code that obtains and tests API endpoint responses and returns status 200 when accessed in browser in "indexSpec.ts" file to test if files can receive data
    
    Ran all five scripts in "package.json" file to test if code runs successfully

--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -1,4 +1,24 @@
 // Tests for "index.js" File
 
-// TEMPORARY CODE
-console.log('Inside indexSpec.ts File');
+// Imports "app" Object FROM "index.ts" Fi
+import app from '../index';
+
+// Imports Super Tests
+import supertest from 'supertest';
+
+// Runs Endpoint Tests on "app"
+// Tells "supertest" What Endpoint Tests are Running On
+const request = supertest(app);
+
+// Test for API Endpoint (suite)
+describe('Test endpoint responses', () => {
+  // Endpoint Tests are ALWAYS Asynchronous
+  it('gets API Endpoint', async () => {
+    // Creates Response that Awaits Super Test Request and Obtains Endpoint API
+    const response = await request.get('/api');
+
+    // Returns Status 200 when Accessed in Browser
+    // Expect Statement that Checks if Response Status is 200 (HTTP Status Code for "okay", Test Passes)
+    expect(response.status).toBe(200);
+  });
+});
