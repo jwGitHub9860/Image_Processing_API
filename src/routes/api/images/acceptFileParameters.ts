77f18ba94a9f1a4075f2c15cd79fc45f98b590bc
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2026-03-28 14:48:01 -0400

    feat: Configure Multer for file uploads
    Add code that configures Multer in "acceptFileParameters.ts" file to store file uploads in "resized-images" folder
    
    This is "Step 3" of method to redefine URLs of images to open images via web browser when image URL is dragged and dropped into web browser URL

--- src/routes/api/images/acceptFileParameters.ts
+++ src/routes/api/images/acceptFileParameters.ts
@@ -14,6 +14,22 @@ fileParameters.use(cors())
 fileParameters.use(express.json())
 fileParameters.use(express.urlencoded({ extended: true }))
 
+// Configure Multer for File Storage
+const storage = multer.diskStorage({
+  // MUST USE "req: any", "file: any", & "cb:any"
+  // Using ONLY "req", "file", & "cb" will Cause Errors
+  destination: function(req: any, file: any, cb:any) {
+    // Ensure "original-images" Directory Exists
+    cb(null, "original-images")
+  },
+  filename: function (req: any, file: any, cb:any) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1E9)
+    cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
+  }
+});
+
+const upload = multer({ storage: storage });
+
 // TEMP: use as test for testing images with NEW PARAMETERS?
 // TEMP: use Either "/" OR "/acceptFileParameters"
 fileParameters.get('/', (req, res) => {
