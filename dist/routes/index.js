c4b62fa04e95e850a7d020497c42c58163bdbe79
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2026-03-30 20:36:14 -0400

    feat: Change message to only display available image filenames
    Change message in "get endpoint" in "index.ts" file in "routes" folder to only tell user what images are available and what their filenames are

--- dist/routes/index.js
+++ dist/routes/index.js
@@ -12,7 +12,7 @@ const routes = express_1.default.Router();
 // Calls Router, Obtains Root Path, and Sends Response from Server of 'Main API Route'
 // Links to Code that Connects ALL Routes with Endpoints to Server File in "index.ts" File in "src" Folder
 routes.get('/', (req, res) => {
-    res.send('Please enter URL similar to the following example: "http://localhost:5000/api/images?filename=encenadaport&width=200&height=200". The available image filenames are the following: "encenadaport.jpg", "fjord.jpg", "icelandwaterfall.jpg", "palmtunnel.jpg", and "santamonica.jpg".');
+    res.send('The available image filenames are the following: "encenadaport.jpg", "fjord.jpg", "icelandwaterfall.jpg", "palmtunnel.jpg", and "santamonica.jpg".');
 });
 // Enables Use of "fileParameters" Routes
 routes.use('/', acceptFileParameters_1.default);
